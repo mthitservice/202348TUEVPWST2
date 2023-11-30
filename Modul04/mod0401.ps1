23776f2b719e1dbf55c3290402c19ba736985ccf
Michael Lindner <Michael.Lindner@mth-it-service.com>
2023-11-30 16:52:27 +0100

    Try

---
+++ Modul04/mod0401.ps1
@@ -0,0 +1,38 @@
+# Fehlerbehandlung
+
+# Eingabe
+# Deklaration und Initialisierung  
+#$wert="start"
+# $zahl=0
+
+function Start-Tuev {
+    throw "Absturz"
+}
+
+
+# Verarbeitung
+try {
+    
+    Start-Tuev
+
+}
+catch {
+   Write-Error -Message "Wir haben ein Problem" -ErrorAction Stop
+   Write-Output $_
+}
+finally {
+    Write-Output "Schließe alles"
+}
+
+
+# Zweiter Testbereich
+try {
+    
+    Write-Output "Test"
+
+}
+catch {
+   Write-Error -Message "Wir haben noch ein Problem" -ErrorAction Stop
+   
+}
+
